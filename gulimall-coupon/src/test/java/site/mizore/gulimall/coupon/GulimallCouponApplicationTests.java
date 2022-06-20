3e721ababd48a66a53e696f984fffc63ff3eac7e
mizoreyo <mizoreyo@outlook.com>
2022-06-20 10:14:20 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/site/mizore/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mizore.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
